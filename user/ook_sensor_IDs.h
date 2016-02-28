209a4828853a2444cc3d299b74a4b57e3fdd3ac9
Billy <sdfghjkl144@gmail.com>
2016-02-28 20:50:16 +1100

    added sensor id file

---
+++ user/ook_sensor_IDs.h
@@ -0,0 +1,14 @@
+#ifndef OOK_SENSOR_IDS_H
+#define OOK_SENSOR_IDS_H
+
+const uint32 recognisedIDs[] = {
+    0x100000,
+    0x200000
+};
+
+const char* recognisedIDsLookup[] = {
+    "example sensor name 1",
+    "example sensor name 2"
+};
+
+#endif
