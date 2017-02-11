943e331e58f299c4199e771be463cc4d8e905d62
Billy <billy@localhost.localdomain>
2017-02-11 16:43:17 +1100

    starting to implement back-and-forth of smtp

--- user/email.c
+++ user/email.c
@@ -16,12 +16,19 @@
 */
 
 extern os_event_t user_procTaskQueue[user_procTaskQueueLen];
+
 struct espconn* emailServerConn;
+char* emailContents[500];
+SMTP_connec_status currentSMTPStatus;
 
 void email_ssl_disconnect()
 {
-    int result = espconn_secure_disconnect(emailServerConn);
-    os_printf("result of disconnect: %d\r\n", result);
+    int errorCode = espconn_secure_disconnect(emailServerConn);
+    os_printf("result of disconnect: %d\r\n", errorCode);
+    if (errorCode)
+        currentSMTPStatus = ERROR;
+    else
+        currentSMTPStatus = SENT;
     system_os_post(user_procTaskPrio, 0, 0);
 }
 
@@ -34,7 +41,7 @@ void init_email()
     emailServerConn->proto.tcp = (esp_tcp*) os_zalloc(sizeof(esp_tcp));
     emailServerConn->proto.tcp->remote_port = EMAIL_PORT;
 
-    espconn_secure_set_size(ESPCONN_CLIENT, 5121);
+    espconn_secure_set_size(ESPCONN_CLIENT, 8000);
     // remove later, note the 0x1 is for SSL client:
     espconn_secure_ca_disable(0x3);
     // espconn_secure_ca_enable(0x1);
@@ -52,7 +59,11 @@ void email_server_dns_lookup_cb(const char *name, ip_addr_t *ipaddr,
                                 void *callback_arg)
 {
     if (ipaddr == NULL || name == NULL)
+    {
+        currentSMTPStatus = ERROR;
+        os_printf("DNS lookup failed");
         return;
+    }
 
     uint8_t* ipBytes = (uint8_t*) ipaddr;
     os_printf("%s found at IP: %d.%d.%d.%d\r\n", name, ipBytes[0], ipBytes[1],
@@ -61,14 +72,71 @@ void email_server_dns_lookup_cb(const char *name, ip_addr_t *ipaddr,
 
     //uint8_t debugIP[4] = {192,168,0,5};
     //os_memcpy(emailServerConn->proto.tcp->remote_ip, debugIP, 4);
-    send_email("test", "hello world");
+    send_email("billy.woods109@gmail.com", "test", "hello world");
+}
+
+void on_connect_cb(void* arg);
+void on_receive_cb(void* arg, char* data, unsigned short dataLen);
+
+void SMTP_client_loop()
+{
+    switch (currentSMTPStatus)
+    {
+        case ERROR:
+            // do nothing
+            return;
+        // will require changes to lookup cb before this case works properly 
+        case DNS_LOOKUP:
+            espconn_gethostbyname(emailServerConn, EMAIL_SERVER, (ip_addr_t*) emailServerConn->proto.tcp->remote_ip,
+                                  email_server_dns_lookup_cb);
+            break;
+
+        case CONNECTING:
+            espconn_regist_connectcb(emailServerConn, on_connect_cb);
+            espconn_regist_recvcb(emailServerConn, on_receive_cb);
+            int secureConnectError = espconn_secure_connect(emailServerConn);
+            //os_printf("result of secure espconn: %d\r\n", secureConnectError);
+
+            if (secureConnectError)
+                currentSMTPStatus = ERROR;
+            break;
+
+        case EHLO:
+            os_printf("got to ehlo\n");
+            break;
+        case AUTH_PLAIN:
+            break;
+        case MAIL_FROM:
+            break;
+        case RCPT_TO:
+            break;
+        case DATA:
+            break;
+        case QUIT:
+            break;
+        case CLOSE_SSL:
+            system_os_post(ssl_disconnectPrio, 0, 0);
+            break;
+        case SENT:
+            // do nothing
+            return;
+    }
+
 }
 
 void on_connect_cb(void* arg)
 {
     struct espconn* conn = (struct espconn*) arg;
     os_printf("connected (cb called!)");
-    system_os_post(ssl_disconnectPrio, 0, 0);
+    currentSMTPStatus = EHLO;
+    SMTP_client_loop();
+}
+
+void on_receive_cb(void* arg, char* data, unsigned short dataLen)
+{
+    os_printf("received from SMTP serv: %s\n", data);
+    currentSMTPStatus += 1;
+    SMTP_client_loop();
 }
 
 void print_espconn_config(struct espconn* conn)
@@ -77,18 +145,27 @@ void print_espconn_config(struct espconn* conn)
     os_printf("remote port: %d\r\n", conn->proto.tcp->remote_port);
 }
 
-bool send_email(char* subject, char* body)
+bool send_email(char* recipient, char* subject, char* body)
 {
+    os_sprintf(emailContents, "From: \"%s\" <%s>\nTo: <%s>\nSubject: %s\n\n%s\n.\r\n\r\n",
+        EMAIL_SENDER, EMAIL_ADDRESS, recipient, subject, body);
+    os_printf(emailContents);
     os_printf("connect cb registered? %d\r\n", espconn_regist_connectcb(emailServerConn, on_connect_cb));
-    int result1 = espconn_secure_connect(emailServerConn);
+    int secureConnectError = espconn_secure_connect(emailServerConn);
     print_espconn_config(emailServerConn);
-    os_printf("result of secure espconn: %d\r\n", result1);
+    os_printf("result of secure espconn: %d\r\n", secureConnectError);
+
+    if (secureConnectError)
+        currentSMTPStatus = ERROR;
+    else
+        currentSMTPStatus = CONNECTING;
 }
 
 void email_debug_func()
 {
     ip_addr_t emailServerIP;
     // perform DNS lookup on host
-    espconn_gethostbyname(emailServerConn, EMAIL_SERVER, (ip_addr_t*) emailServerConn->proto.tcp->remote_ip,
-                          email_server_dns_lookup_cb);
+    os_printf("dns lookup: %d\n", espconn_gethostbyname(emailServerConn, EMAIL_SERVER, (ip_addr_t*) emailServerConn->proto.tcp->remote_ip,
+                          email_server_dns_lookup_cb));
+    currentSMTPStatus = DNS_LOOKUP;
 }
--- user/email.h
+++ user/email.h
@@ -1,14 +1,32 @@
 #ifndef _EMAIL_H
 #define _EMAIL_H
 
-#define EMAIL_ADDRESS 
-#define EMAIL_PASSWORD
-#define EMAIL_SERVER 
-#define EMAIL_PORT 
+#define EMAIL_ADDRESS "username"
+#define EMAIL_SENDER "ESP8266 alarm"
+#define EMAIL_PASSWORD "password"
+// auth string generated in python via base64.b64encode(b"\000<email_address>\000<email_password>")
+#define EMAIL_AUTH_STRING ""
+#define EMAIL_SERVER "smtp.gmail.com"
+#define EMAIL_PORT 465
 //const char* cert
 
 void init_email();
 void email_debug_func();
-bool send_email(char* subject, char* body);
+bool send_email(char* recipient, char* subject, char* body);
+
+typedef enum
+{
+    ERROR,
+    DNS_LOOKUP,
+    CONNECTING,
+    EHLO,
+    AUTH_PLAIN,
+    MAIL_FROM,
+    RCPT_TO,
+    DATA,
+    QUIT,
+    CLOSE_SSL,
+    SENT
+}SMTP_connec_status;
 
 #endif
